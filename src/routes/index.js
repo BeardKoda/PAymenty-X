522c599b6d235f2b2f3473c76c1092a05f4ebb4a
BeardKoda <beardkoda@BeardKoda.local>
2019-12-29 03:15:48 +0100

    Lates

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,18 +1,30 @@
 const appRoute = (app)=>{
     app.get('/', (req, res)=>{
         console.log(app.mountpath)
-        // res.send('Landing Page')÷
-        res.render('front/land')
+        res.render('front/land', {title:"Home"})
     })
+    app.get('/privacy-policy', (req, res)=>{
+        console.log(app.mountpath)
+        res.render('front/privacy', {title:"Privacy Policy"})
+    })
+    app.get('/terms-condition', (req, res)=>{
+
+        res.render('front/terms', {title:"Terms and Conditions"})
+    })
+    app.get('/contact', (req, res)=>{
+        res.render('front/contact', {title:"Contact"})
+    })
+
+    // handle 404
     app.get('**', (req,res,next)=>{
         res.render('front/error/404')
     })
     
     // Handle 500
-    app.use(function(error, req, res, next) {
-        res.status(500);
-        res.render('error/500', {title:'500: Internal Server Error', error: error});
-    });
+    // app.use(function(error, req, res, next) {
+    //     res.status(500);
+    //     res.render('error/500', {title:'500: Internal Server Error', error: error});
+    // });
 }
 
 module.exports = appRoute;
